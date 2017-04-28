f74138f9e399fe46ce5144e3df31b0ffe5d25387
Josh Jackson <jjackson2246@yahoo.com>
2017-04-28 09:02:04 -0700

    removing spacing at the top of pdp header and adding the tracking pixel for GA before I try the Zarget integration

---
+++ trackingpixels-4.27.js
@@ -0,0 +1,91 @@
+ <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-34751039-1', 'www.medcline.com');
+    ga('send', 'pageview');
+    ga('require', 'ecommerce', 'ecommerce.js');
+
+    function trackEcommerce() {
+    this._addTrans = addTrans;
+    this._addItem = addItems;
+    this._trackTrans = trackTrans;
+    }
+    function addTrans(orderID,store,total,tax,shipping,city,state,country) {
+    ga('ecommerce:addTransaction', {
+        'id': orderID,
+        'affiliation': store,
+        'revenue': total,
+        'tax': tax,
+        'shipping': shipping,
+        'city': city,
+        'state': state,
+        'country': country
+    });
+    }
+    function addItems(orderID,sku,product,variation,price,qty) {
+    ga('ecommerce:addItem', {
+        'id': orderID,
+        'sku': sku,
+        'name': product,
+        'category': variation,
+        'price': price,
+        'quantity': qty
+    });
+    }
+    function trackTrans() {
+        ga('ecommerce:send');
+    }
+    var pageTracker = new trackEcommerce();
+</script>
+
+<meta name="msvalidate.01" content="1E208D4A00D9740D351B8DA6A17B5113" />
+		<!--<title>Your SEO optimized title</title>-->
+
+<!-- Hotjar Tracking Code for www.medcline.com -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:396425,hjsv:5};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+
+<!-- Facebook Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+document,'script','https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '252466351811686', {
+em: 'insert_email_variable,'
+});
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=252466351811686&ev=PageView&noscript=1"
+/></noscript>
+<!-- DO NOT MODIFY -->
+<!-- End Facebook Pixel Code -->
+
+<!-- Start of LiveChat (www.livechatinc.com) code -->
+<script type="text/javascript">
+window.__lc = window.__lc || {};
+window.__lc.license = 8737016;
+(function() {
+  var lc = document.createElement('script'); lc.type = 'text/javascript'; lc.async = true;
+  lc.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.livechatinc.com/tracking.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(lc, s);
+})();
+</script>
+<!-- End of LiveChat code -->
+
+<!-- Begin Zarget Code -->
+<script src='//cdn.zarget.com/145333/342519.js'></script>
+<!-- End Zarget Code -->
