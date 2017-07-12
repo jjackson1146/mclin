348750e95f7cff20bf0c0b918e6c4a403d2e6ed0
jjackson1146 <jjackson2246@yahoo.com>
2017-07-12 12:42:47 -0700

    hi

---
+++ cart-redorect-bigcommerce.php
@@ -0,0 +1,11 @@
+<script>   
+jQuery(document).ready(function(){
+$('#addToCartButton').on('click', function(c){
+setTimeout(function(){
+ window.location.href = "https://www.medcline.com/cart.php";
+},7000);
+    
+}); 
+});
+
+</script>
---
+++ footerCode.js
@@ -0,0 +1,269 @@
+<style>
+.prodiv11  { width: 45%;float:left; margin-right: 7%;margin-left: 2%;}
+.prodiv22  {width: 45%;float: left;}
+.clic-img1 {float: left;display: block;padding: 0px 40px 0px 100px;}
+.clic-cont1 {padding-top: 1px;}
+.clic-cont1 h3 {font-weight: bold;}
+.SleBoxMid11.prof {float: left;}
+.set {text-transform:uppercase; font-size: 1.7rem; margin-top:5px;}
+.set2  {text-transform:uppercase; font-size: 1.7rem;  }
+.bg {background:#F7F7F7;}
+.frm {text-align: center;margin: 0 auto;}
+.pos{ display: inline-block;text-align: left;}
+.bts {display: inline-block;width: 100%;text-align: center;margin-bottom: 60px;}
+.clic-cont1 a {font-weight: bold;}
+.clic-cont1 h3 {font-weight: bold;margin-bottom: 0;margin-top: 8px;}
+.clic-cont1 p {margin-top: 3px;}
+.clic-img1 img {max-width: 150px;}
+.pd {margin-bottom:40px; }  
+.acrdn {max-width: 625px;}  
+.modal {position: static;z-index: 1;padding-top:0px;left: 0;top: 0;width: 100%;height: 100%;overflow: visible;
+display:block;background-color: transparent;}
+.modal-narrow .modal-content {padding: 6.375rem;position: relative;}
+.form-pagebreak-back, .form-pagebreak-next, .qq-upload-button, .form-submit-button, .form-submit-reset, .form-submit-print {
+    border: 1px solid #fff!important;
+   padding: 1.4375rem 2.125rem 1.375rem!important;;
+    font-family: "Roboto Condensed"!important;;
+    font-size: 16px!important;;
+    font-weight: 500!important;;
+    line-height: 1.154!important;;
+    letter-spacing: .125rem!important;;
+    color: #fff!important;;
+    text-transform: uppercase!important;;
+    text-align: center!important;;
+    background-color: #F79933!important;;
+    box-shadow: 0 0 0 1px transparent inset!important;;
+    transition: all .2s cubic-bezier(.25,.46,.45,.94)!important;;
+ 
+}
+.form-footer {
+    display: none;
+}
+.supernova .form-all, .form-all {
+    background-color: #fff;
+    border: 1px solid transparent;
+}
+
+.alert-close {cursor:pointer;}
+</style>
+
+<style type="text/css">
+.klaviyo_condensed_styling label, .klaviyo_styling label {
+    font-size: 16px;
+    line-height: 1.2;
+    text-align: left;
+    margin-bottom: 6px;
+    display: block;
+    font-weight: 400;
+    -webkit-font-smoothing: antialiased;
+}
+    
+    
+    .klaviyo_condensed_styling, .klaviyo_styling {
+   background:#fff;
+    max-width: 340px;
+}
+    
+    
+.frm1 {
+    background: #fff;
+    padding: 20px;
+    float: none;
+    width: 100%;
+    text-align: center;
+    margin: 0 auto;
+}
+.pdown {    margin-top: 8%;}
+    
+    .mobi.message.success.show {
+    display: none!important;
+}   
+    
+ h1.carthead {    font-family: "Roboto Condensed";
+    font-size: 1.5rem;
+    font-weight: 300;
+    line-height: 1.333;}   
+    
+
+  @media only screen and (max-width : 991px) 
+{
+
+.clic-cont1 h3 {font-size: 20px;}
+  .mob {text-align:center;}  
+
+}
+    
+    
+ @media only screen and (max-width : 767px) 
+
+{
+.clic-img1 img {
+    max-width: 130px;
+}
+  .clic-cont1 h3 {
+    font-size: 18px;
+        margin-top:0px;
+}
+   .clic-cont1 p {
+    margin-top: 0;
+    font-size: 15px;
+}
+.clic-img1 {
+  
+    padding: 0px 20px 0px 15px;
+}
+    
+    
+    
+}
+    
+    
+ @media only screen and (max-width : 767px) 
+ 
+    {
+        
+        .popup {
+    margin: 70px auto;
+        padding: 10px 10px 2px 10px;
+    background: #fff;
+    border-radius: 5px;
+    width: 90%;
+    position: relative;
+    transition: all 5s ease-in-out;
+}
+        
+    .clic-img1 img {
+    max-width: 130px;
+    margin-bottom: 5px;
+}
+    .clic-img1 {
+    padding: 0px 20px 0px 15px;
+    float: none;
+    padding-left: 0px;
+}
+    
+.SleBoxMid11.prof {
+    float: left;
+    margin-bottom: 20px;
+}
+    
+  .alert-close {
+    cursor: pointer;
+    float: right;
+}
+
+.mobi.message.success.show {
+    display: block!important;
+    overflow: auto;
+    width: 94%;
+    position: fixed;
+    bottom: 80px;
+    margin: :0 auto;
+    display: inline-block;
+    left: 50%;
+    transform: translateX(-50%);
+    z-index: 99;
+    text-align: center;
+}
+    
+</style>
+
+<style type="text/css">
+.klaviyo_styling.klaviyo_standard_embed_NmnVYY,
+.klaviyo_condensed_styling.klaviyo_standard_embed_NmnVYY {
+  font-family: inherit;
+}.klaviyo_styling.klaviyo_standard_embed_NmnVYY label,
+.klaviyo_condensed_styling.klaviyo_standard_embed_NmnVYY label {
+  
+}.klaviyo_styling.klaviyo_standard_embed_NmnVYY input[type=text],
+.klaviyo_styling.klaviyo_standard_embed_NmnVYY input[type=email],
+.klaviyo_condensed_styling.klaviyo_standard_embed_NmnVYY input[type=text],
+.klaviyo_condensed_styling.klaviyo_standard_embed_NmnVYY input[type=email] {
+  border-radius: undefined;
+}.klaviyo_styling.klaviyo_standard_embed_NmnVYY .klaviyo_submit_button,
+.klaviyo_condensed_styling.klaviyo_standard_embed_NmnVYY .klaviyo_submit_button {
+  
+  border-radius: undefined;
+}.klaviyo_styling.klaviyo_standard_embed_NmnVYY .klaviyo_submit_button:hover,
+.klaviyo_condensed_styling.klaviyo_standard_embed_NmnVYY .klaviyo_submit_button:hover {
+  
+}
+.klaviyo_condensed_styling .klaviyo_messages .error_message, .klaviyo_condensed_styling .klaviyo_messages .success_message, .klaviyo_styling .klaviyo_messages .error_message, .klaviyo_styling .klaviyo_messages .success_message {
+    font-size: 16px;
+    line-height: 1.5;
+    margin-bottom: 12px;
+}
+.frm {
+    background: #fff;
+    padding: 0px;
+    float: left;
+    width: 66%;
+}
+#igFlag {
+ 
+    float: left;
+}
+    
+    
+ 
+ 
+</style>
+
+<script type="text/javascript" src="//www.klaviyo.com/media/js/public/klaviyo_subscribe.js"></script>
+
+<script type="text/javascript">
+  KlaviyoSubscribe.attachToForms('#email_signup', {
+    hide_form_on_success: false,
+      success_message: "Thank you for requesting patient brochures! We will get your brochures shipped out to you right away."
+  }); 
+    
+</script>
+<script>   
+jQuery(document).ready(function(){
+jQuery(".sn_btn").click(function(){
+if(jQuery("#k_id_email").val()!='')
+{
+    setTimeout(function(){
+jQuery(".sn_msg").show();
+        },2000);
+}
+  });
+
+    });
+</script>
+
+<script>
+    
+    $(document).ready(function(c) {
+    $('.alert-close').on('click', function(c){
+        $('.message').fadeOut('slow', function(c){
+              $('.message').remove();
+        });
+    });    
+});
+</script> 
+
+
+
+<script>   
+jQuery(document).ready(function(){
+$('#addToCartButton').on('click', function(c){
+setTimeout(function(){
+ window.location.href = "https://www.medcline.com/cart.php";
+},7000);
+    
+}); 
+});
+
+</script>
+
+
+<div id="popup1" class="overlay">
+<div class="popup"><a class="close" href="#">x</a>
+<div class="content"><iframe src="https://www.youtube.com/embed/ts_odVm3eg8?rel=0" width="100%" height="350" frameborder="0"></iframe></div>
+
+</div>
+</div>
+
+<script type="text/javascript">var spuritCartTotal = "{{cart.sub_total.formatted}}";</script>
+<script type="text/javascript" src="//8upsell.s3.amazonaws.com/bigcommerce/store/initbd4d103a1bcf0a86a17c813328d85189.js"></script>
